4201f99c28359707212d7fcc34cd5c5d30cd8825
Josip <josipkeresman@gmail.com>
2025-05-25 14:02:04 +0200

    Inital commit

---
+++ Editor/src/main/java/com/keresman/editor/Editor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.keresman.editor;
+
+/**
+ *
+ * @author josip
+ */
+public class Editor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
