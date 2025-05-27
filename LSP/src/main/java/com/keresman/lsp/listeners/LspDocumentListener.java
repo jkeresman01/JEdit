2d69182008e9a185ec1a2d8bcee91e5983e3d4e4
Josip <josipkeresman@gmail.com>
2025-05-28 01:04:49 +0200

    Implement LSPDocumentListener

--- LSP/src/main/java/com/keresman/lsp/listeners/LspDocumentListener.java
+++ LSP/src/main/java/com/keresman/lsp/listeners/LspDocumentListener.java
@@ -1,5 +1,60 @@
 package com.keresman.lsp.listeners;
 
-public class LspDocumentListener {
+import com.keresman.lsp.LspClientManager;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import org.eclipse.lsp4j.TextDocumentContentChangeEvent;
+import org.eclipse.lsp4j.VersionedTextDocumentIdentifier;
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
 
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+
+public class LspDocumentListener implements DocumentListener {
+
+    private final String uri;
+    private final LspClientManager lspClientManager;
+    private int version = 1;
+
+    public LspDocumentListener(String uri, LspClientManager lspClientManager) {
+        this.uri = uri;
+        this.lspClientManager = lspClientManager;
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        sendChange(e.getDocument());
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        sendChange(e.getDocument());
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        sendChange(e.getDocument());
+    }
+
+    private void sendChange(Document doc) {
+        String text = "";
+
+        try {
+            text = doc.getText(0, doc.getLength());
+        } catch (BadLocationException ex) {
+            Logger.getLogger(LspDocumentListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        var identifier = new VersionedTextDocumentIdentifier();
+        identifier.setUri(uri);
+        identifier.setVersion(version++);
+
+        var change = new TextDocumentContentChangeEvent(text);
+        var params = new DidChangeTextDocumentParams(identifier, Collections.singletonList(change));
+
+        lspClientManager.getTextDocumentService().didChange(params);
+    }
 }
